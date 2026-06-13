a085d88c7e24b69676177b8104d19b736a75d20b
abananaj <anna@theatrum.dev>
2026-06-12 17:18:29 -0700

    deploy update

--- build/blocks/popup/index.asset.php
+++ build/blocks/popup/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd5709b321b1b63c69043');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'da41b952a869bf03bd3c');
