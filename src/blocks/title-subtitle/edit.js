9d087fc06869a4fbe72ed222644174c40317aca9
abananaj <anna@theatrum.dev>
2026-06-17 00:29:15 -0700

    refactor title subtitle

---
+++ src/blocks/title-subtitle/edit.js
@@ -0,0 +1,21 @@
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+
+const TEMPLATE = [
+	[ 'core/heading', { level: 1, placeholder: 'Title' } ],
+	[ 'core/heading', { level: 2, placeholder: 'Subtitle' } ],
+];
+
+const ALLOWED_BLOCKS = [ 'core/heading' ];
+
+export default function Edit() {
+	const blockProps = useBlockProps();
+
+	return (
+		<div { ...blockProps }>
+			<InnerBlocks
+				template={ TEMPLATE }
+				allowedBlocks={ ALLOWED_BLOCKS }
+			/>
+		</div>
+	);
+}
