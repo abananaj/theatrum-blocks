f5d5ac35b96045e7dce4fb796278bda046d43627
abananaj <anna@theatrum.dev>
2026-06-30 00:47:24 -0700

    remove examples

--- src/blocks/_examples/basic-editable-block-no-build-1b8c51/block.asset.php
+++
@@ -1,4 +0,0 @@
-<?php return [
-    "dependencies" => ["react", "wp-block-editor", "wp-blocks"],
-    "version" => "jns4hjf256k63try7oq4",
-];
--- src/blocks/_examples/dynamic-block/view.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable no-console */
-console.log(
-	'Hello World! (from block-development-examples-dynamic-block block)'
-);
-/* eslint-enable no-console */
