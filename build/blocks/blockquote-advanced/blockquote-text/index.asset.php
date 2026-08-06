5ae1e6e714dafb5ee4cedbec53c55ecaa7399ed7
abananaj <anna@theatrum.dev>
2026-08-06 02:17:21 -0700

    Refactor blockquote components: update attributes and styles for improved structure and appearance

--- build/blocks/blockquote-advanced/blockquote-text/index.asset.php
+++ build/blocks/blockquote-advanced/blockquote-text/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0211aea0391ca567ee15');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b46698ff20d9a052e7a6');
